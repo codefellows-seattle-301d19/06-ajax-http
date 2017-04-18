d735bf16f2cf6a285caaf1c8bb01e9a45bc321ec
Christina Thomas <Penssake@gmail.com>
2017-04-18 11:24:53 -0600

    Finished last TODO, pair-programming

--- james-christina/scripts/article.js
+++ james-christina/scripts/article.js
@@ -50,6 +50,7 @@ Article.fetchAll = function() {
     // When rawData is already in localStorage,
     // we can load it with the .loadAll function above,
     // and then render the index page (using the proper method on the articleView object).
+    console.log(JSON.parse(localStorage.rawData));
     Article.loadAll(JSON.parse(localStorage.rawData)); //TODO: What do we pass in to loadAll()?
     //TODO: What method do we call to render the index page?
     articleView.initIndexPage();
@@ -60,10 +61,10 @@ Article.fetchAll = function() {
     // cache it in localStorage so we can skip the server call next time,
     // then load all the data into Article.all with the .loadAll function above,
     // and then render the index page.
-    $.get('data/hackeripsum.json', function( rawData) {
-      localStorage.rawData = $('#article-json').text(JSON.stringify(rawData));
+    $.get('data/hackeripsum.json', function(rawData) {
+      Article.loadAll(rawData);
+      localStorage.rawData = JSON.stringify(rawData);
       // Article.loadAll(JSON.parse(localStorage.rawData));
-      });
-    };
+    });
   }
-
+}
